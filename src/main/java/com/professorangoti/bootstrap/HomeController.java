a7ecc6624095ce02bfe69f69e77b9afb303b549b
Lardenon <ryann.magalhaes@estudante.iftm.edu.br>
2022-09-15 21:54:48 -0300

    Segunda Etapa

--- src/main/java/com/professorangoti/bootstrap/HomeController.java
+++ src/main/java/com/professorangoti/bootstrap/HomeController.java
@@ -1,5 +1,10 @@
 package com.professorangoti.bootstrap;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
-    
+    @GetMapping("home")
+    public String home(){
+        return "home";
+    }
 }
